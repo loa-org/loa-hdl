521c8029a77854bb276e22d602f338ceba631090
cajt <cjt@users.sf.net>
2025-11-21 19:23:44 +0100

    SVN seemed to fail. This works better.

--- modules/spw_node/fetch_spacewire_light.sh
+++ modules/spw_node/fetch_spacewire_light.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-svn co http://opencores.org/ocsvn/spacewire_light/spacewire_light/trunk/ vendor 
+#svn co http://opencores.org/ocsvn/spacewire_light/spacewire_light/trunk/ vendor 
+git clone https://github.com/freecores/spacewire_light.git vendor
